200bf355dcdd252621a22c6a32ff7bded786128b
Habeeb O OLAYEMI <49318907+olufemi92@users.noreply.github.com>
2023-05-29 15:37:49 +0100

    Update main.tf

--- remote-state/main.tf
+++ remote-state/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
-  profile= "yusuf"
+  region = "us-east-2"
+  profile= "captainnlt"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "yusuf-docker-tfstate"
+  bucket = "pest111-docker-tfstate"
      
   lifecycle {
     prevent_destroy = true
